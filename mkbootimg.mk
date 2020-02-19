d48c1f86f63443569c9c405729f8c7261f11a9f8
steadfasterX <steadfasterX@gmail.com>
2020-02-19 17:11:06 +0100

    mk: fixing all issues with mkbootimg
    requires an if statement for BOARD_PROVIDES_MKBOOTIMG
    in system/core/mkbootimg/Android.mk to get fully applied
    
    Signed-off-by: steadfasterX <steadfasterX@gmail.com>

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,7 +1,12 @@
 LOCAL_PATH := $(call my-dir)
 
-MKBOOTIMG_TOOL :=  $(HOST_OUT_EXECUTABLES)/degas-mkbootimg
-DTBTOOL := 	./scripts/mkdtimg.sh
+# custom mkbootimg
+BOARD_PROVIDES_MKBOOTIMG := true
+MKBOOTIMG := $(HOST_OUT_EXECUTABLES)/degas-mkbootimg
+# override unsupported args
+INTERNAL_MKBOOTIMG_VERSION_ARGS :=
+
+DTBTOOL := ./scripts/mkdtimg.sh
 INSTALLED_DTIMAGE_TARGET := $(TOP)/device/samsung/gtexslte/dt.img
 INSTALLED_SEANDROID_IMAGE := $(TOP)/device/samsung/gtexslte/seandroid.img
 
@@ -11,17 +16,17 @@ INSTALLED_SEANDROID_IMAGE := $(TOP)/device/samsung/gtexslte/seandroid.img
 # 	$(hide) cd $(TOP)/kernel/samsung/gtexslte && $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -p $(KERNEL_OUT) -i $(TOP)/kernel/samsung/gtexslte/arch/arm/boot/dts/
 # 	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG_TOOL) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(INSTALLED_SEANDROID_IMAGE)
-	$(call pretty,"Target boot image: $@  ${MKBOOTIMG_TOOL} ${INTERNAL_BOOTIMAGE_ARGS} ${BOARD_MKBOOTIMG_ARGS}")
-	$(hide) $(MKBOOTIMG_TOOL) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --signature $(INSTALLED_SEANDROID_IMAGE) --output $@
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET) $(INSTALLED_SEANDROID_IMAGE)
+	$(call pretty,"Target boot image: $@  ${MKBOOTIMG} ${INTERNAL_BOOTIMAGE_ARGS} ${BOARD_MKBOOTIMG_ARGS}")
+	$(hide) $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --dt $(INSTALLED_DTIMAGE_TARGET) --signature $(INSTALLED_SEANDROID_IMAGE) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made boot image: $@"${CL_RST}
 
 ## Overload recoveryimg generation: Same as the original, + --dt arg
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG_TOOL) $(INSTALLED_DTIMAGE_TARGET) \
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(INSTALLED_DTIMAGE_TARGET) \
 		$(recovery_ramdisk) \
 		$(recovery_kernel)
 	@echo -e ${CL_CYN}"----- Making recovery image ------"${CL_RST}
-	$(hide) $(MKBOOTIMG_TOOL) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_RECOVERYIMAGE_PARTITION_SIZE),raw)
 	@echo -e ${CL_CYN}"Made recovery image: $@"${CL_RST}
